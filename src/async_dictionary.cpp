f3eae675c9fe575831095731f1865f05202f31c0
julien.gautier <julien.gautier@epita.fr>
2018-06-13 11:03:58 +0100

    add: async_dictionary add reader, insert and eraser variable: no query can start until queries from other type have finished to insure sequential consistency

--- src/async_dictionary.cpp
+++ src/async_dictionary.cpp
@@ -1,9 +1,14 @@
 #include "async_dictionary.hpp"
 
-async_dictionary::async_dictionary(
-    const std::initializer_list<std::string>& init)
-	: m_dic(init)
-{}
+async_dictionary::async_dictionary()
+ : reader(0), inserter(0), eraser(0)
+{
+}
+
+async_dictionary::async_dictionary(const std::initializer_list<std::string>& init)
+  : reader(0), inserter(0), eraser(0), m_dic(init)
+{
+}
 
 void async_dictionary::init(const std::vector<std::string>& word_list)
 {
@@ -12,23 +17,71 @@ void async_dictionary::init(const std::vector<std::string>& word_list)
 
 std::future<result_t> async_dictionary::search(const std::string& query) const
 {
-	std::promise<result_t> p;
-	p.set_value(m_dic.search(query));
-	return p.get_future();
+	{
+		std::unique_lock l(m);
+		cv.wait(l, [this] {
+			if (inserter == 0 && eraser == 0) {
+				reader++;
+				return true;
+			}
+			return false;
+		});
+	}
+
+	
+	return std::async(std::launch::async, [this, query]{
+		auto res = m_dic.search(query);
+		{
+			std::lock_guard g(m);
+			reader--;
+		}
+		this->cv.notify_all();
+		return res;
+	});
 }
 
 std::future<void> async_dictionary::insert(const std::string& w)
 {
-	std::promise<void> p;
-	m_dic.insert(w);
-	p.set_value();
-	return p.get_future();
+	{
+		std::unique_lock l(m);
+		cv.wait(l, [this] { 
+			if (!reader && !eraser) {
+				inserter++;
+				return true;
+			}
+			return false;
+		});
+	}
+
+	return std::async(std::launch::async, [this, w]{
+		m_dic.insert(w);
+		{
+			std::lock_guard g(m);
+			inserter--;
+		}
+		this->cv.notify_all();
+	});
 }
 
 std::future<void> async_dictionary::erase(const std::string& w)
 {
-	std::promise<void> p;
-	m_dic.erase(w);
-	p.set_value();
-	return p.get_future();
+	{
+  std::unique_lock l(m);
+	cv.wait(l, [this] {
+		if (!reader && !inserter) {
+			eraser++;
+			return true;
+		}
+		return false;
+	});
+	}
+
+	return std::async(std::launch::async, [this, w]{
+		m_dic.erase(w);
+		{
+			std::lock_guard g(m);
+			eraser--;
+		}
+		this->cv.notify_all();
+	});
 }
--- src/async_dictionary.hpp
+++ src/async_dictionary.hpp
@@ -6,23 +6,28 @@
 /// The naive implementation is blocking to ensure Sequential Consistency
 class async_dictionary : public IAsyncDictionary {
 public:
-	async_dictionary() = default;
+  async_dictionary();
 
-	template <class Iterator>
-	async_dictionary(Iterator begin, Iterator end);
-	async_dictionary(const std::initializer_list<std::string>& init);
-	void init(const std::vector<std::string>& word_list) final;
+  template <class Iterator>
+  async_dictionary(Iterator begin, Iterator end);
+  async_dictionary(const std::initializer_list<std::string>& init);
+  void init(const std::vector<std::string>& word_list) final;
 
-	// clang-format off
-	std::future<result_t>	search(const std::string& w) const final;
-	std::future<void>	insert(const std::string& w) final;
-	std::future<void>	erase(const std::string& w) final;
-	// clang-format on
+  std::future<result_t>      search(const std::string& w) const final;
+  std::future<void>          insert(const std::string& w) final;
+  std::future<void>          erase(const std::string& w) final;
+	
 private:
-	dictionary m_dic;
+	mutable std::mutex m;
+	mutable std::atomic<int> reader;
+	mutable std::atomic<int> inserter;
+	mutable std::atomic<int> eraser;
+	mutable std::condition_variable cv;
+  dictionary m_dic;
 };
 
 template <class Iterator>
 async_dictionary::async_dictionary(Iterator begin, Iterator end)
-	: m_dic(begin, end)
-{}
+  : reader(0), inserter(0), eraser(0), m_dic(begin, end)
+{
+}
